2aed6183f827e0f0050234dcd5c24ce2d807f0aa
enkhee-Osiris <enkhee.ag@gmail.com>
2018-07-28 15:52:05 +0800

    add config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,7 @@
+# Terminate already running instances
+killall -q polybar
+
+# Wait until the processes have been shutdown
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+polybar momiji

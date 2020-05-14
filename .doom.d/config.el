624a67d6667984f15603a5f45a531fe1c202fa53
enkhee-Osiris <enkhee.ag@gmail.com>
2020-05-14 15:13:45 +0800

    chore: add tag creator hook

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,5 +1,36 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
+(defun my-create-tags-if-needed (SRC-DIR &optional FORCE)
+  "return the full path of tags file"
+  (let ((dir (file-name-as-directory (file-truename SRC-DIR)))
+        file)
+    (setq file (concat dir "TAGS"))
+    (when (or FORCE (not (file-exists-p file)))
+      (message "Creating TAGS in %s ..." dir)
+      (shell-command
+       (format "ctags -f %s -e -R %s" file dir))
+      )
+    file
+    ))
+
+(defvar my-tags-updated-time nil)
+
+(defun my-update-tags ()
+  (interactive)
+  "check the tags in tags-table-list and re-create it"
+  (dolist (tag tags-table-list)
+    (my-create-tags-if-needed (file-name-directory tag) t)))
+
+(defun my-auto-update-tags-when-save ()
+  (interactive)
+  (cond
+   ((not my-tags-updated-time)
+    (setq my-tags-updated-time (current-time)))
+   ((> (- (float-time (current-time)) (float-time my-tags-updated-time)) 20)
+    (setq my-tags-updated-time (current-time))
+    (my-update-tags)
+    (message "updated tags after %d seconds." (- (float-time (current-time))  (float-time my-tags-updated-time))))))
+
 (after! org
   (setq org-directory "~/.org/"
         org-agenda-files (list org-directory)
@@ -49,3 +80,8 @@
       "rs"  #'tide-rename-symbol
       "roi" #'tide-organize-imports
       "e"   #'tide-project-errors)
+
+(after! web-mode
+  (set-company-backend! 'web-mode 'company-files 'company-keywords 'company-dabbrev-code 'company-etags 'company-dabbrev))
+
+(add-hook 'after-save-hook 'my-auto-update-tags-when-save)
